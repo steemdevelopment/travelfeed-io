f085d676d381153fae9ad7f7ae566d37ae398753
tiotdev <tiotdev@tiot.de>
2018-12-14 21:41:06 +0800

    Added favicons

--- components/Layout.js
+++ components/Layout.js
@@ -26,6 +26,28 @@ const Layout = props => (
         crossOrigin="anonymous"
       />
       <link rel="stylesheet" href="/style.css" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <Header />
     <div style={{ padding: "20px" }}>{props.children}</div>

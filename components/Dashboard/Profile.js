603acfea08adf7a744d3fa2f81c19c73d87ce23b
tiotdev <tiotdev@tiot.de>
2019-01-06 23:43:56 +0800

    Get current profile from blockchain

--- components/Dashboard/Profile.js
+++ components/Dashboard/Profile.js
@@ -3,8 +3,184 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Helmet from "react-helmet";
+import PropTypes from "prop-types";
+import { Client } from "dsteem";
+import TextField from "@material-ui/core/TextField";
+
+const client = new Client("https://api.steemit.com");
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: this.props.user,
+      profile_image: "",
+      cover_image: "",
+      name: "",
+      about: "",
+      location: "",
+      website: "",
+      facebook: "",
+      instagram: "",
+      youtube: "",
+      changed: []
+    };
+    this.handleEditorChange_profile_image = this.handleEditorChange_profile_image.bind(
+      this
+    );
+    this.handleEditorChange_cover_image = this.handleEditorChange_cover_image.bind(
+      this
+    );
+    this.handleEditorChange_name = this.handleEditorChange_name.bind(this);
+    this.handleEditorChange_about = this.handleEditorChange_about.bind(this);
+    this.handleEditorChange_location = this.handleEditorChange_location.bind(
+      this
+    );
+    this.handleEditorChange_website = this.handleEditorChange_website.bind(
+      this
+    );
+    this.handleEditorChange_facebook = this.handleEditorChange_facebook.bind(
+      this
+    );
+    this.handleEditorChange_instagram = this.handleEditorChange_instagram.bind(
+      this
+    );
+    this.handleEditorChange_youtube = this.handleEditorChange_youtube.bind(
+      this
+    );
+  }
+  handleEditorChange_profile_image(profile_image) {
+    var changed = this.state.changed;
+    if (changed.indexOf("profile_image") > -1 === false) {
+      changed = this.state.changed.concat("profile_image");
+    }
+    this.setState({
+      profile_image: profile_image.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_cover_image(cover_image) {
+    var changed = this.state.changed;
+    if (changed.indexOf("cover_image") > -1 === false) {
+      changed = this.state.changed.concat("cover_image");
+    }
+    this.setState({
+      cover_image: cover_image.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_name(name) {
+    var changed = this.state.changed;
+    if (changed.indexOf("name") > -1 === false) {
+      changed = this.state.changed.concat("name");
+    }
+    this.setState({
+      name: name.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_about(about) {
+    var changed = this.state.changed;
+    if (changed.indexOf("about") > -1 === false) {
+      changed = this.state.changed.concat("about");
+    }
+    this.setState({
+      about: about.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_location(location) {
+    var changed = this.state.changed;
+    if (changed.indexOf("location") > -1 === false) {
+      changed = this.state.changed.concat("location");
+    }
+    this.setState({
+      location: location.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_website(website) {
+    var changed = this.state.changed;
+    if (changed.indexOf("website") > -1 === false) {
+      changed = this.state.changed.concat("website");
+    }
+    this.setState({
+      website: website.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_facebook(facebook) {
+    var changed = this.state.changed;
+    if (changed.indexOf("facebook") > -1 === false) {
+      changed = this.state.changed.concat("facebook");
+    }
+    this.setState({
+      facebook: facebook.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_instagram(instagram) {
+    var changed = this.state.changed;
+    if (changed.indexOf("instagram") > -1 === false) {
+      changed = this.state.changed.concat("instagram");
+    }
+    this.setState({
+      instagram: instagram.target.value,
+      changed: changed
+    });
+  }
+  handleEditorChange_youtube(youtube) {
+    var changed = this.state.changed;
+    if (changed.indexOf("youtube") > -1 === false) {
+      changed = this.state.changed.concat("youtube");
+    }
+    this.setState({
+      youtube: youtube.target.value,
+      changed: changed
+    });
+  }
+  async getProfile() {
+    // TODO: get current author name from permlink
+    const acc = await client.database.getAccounts([this.state.author]);
+    const json = JSON.parse(acc[0].json_metadata);
+    var profile_image =
+      typeof json.profile.profile_image != "undefined"
+        ? json.profile.profile_image
+        : "";
+    var cover_image =
+      typeof json.profile.cover_image != "undefined"
+        ? json.profile.cover_image
+        : "";
+    var name = typeof json.profile.name != "undefined" ? json.profile.name : "";
+    var about =
+      typeof json.profile.about != "undefined" ? json.profile.about : "";
+    var location =
+      typeof json.profile.location != "undefined" ? json.profile.location : "";
+    var website =
+      typeof json.profile.website != "undefined" ? json.profile.website : "";
+    var facebook =
+      typeof json.profile.facebook != "undefined" ? json.profile.facebook : "";
+    var instagram =
+      typeof json.profile.instagram != "undefined"
+        ? json.profile.instagram
+        : "";
+    var youtube =
+      typeof json.profile.youtube != "undefined" ? json.profile.youtube : "";
+    this.setState({
+      profile_image: profile_image,
+      cover_image: cover_image,
+      name: name,
+      about: about,
+      location: location,
+      website: website,
+      facebook: facebook,
+      instagram: instagram,
+      youtube: youtube
+    });
+  }
+  componentDidMount() {
+    this.getProfile();
+  }
   render() {
     return (
       <Fragment>
@@ -21,15 +197,127 @@ class Profile extends Component {
           <Grid item lg={7} md={8} sm={11} xs={12}>
             <Card>
               <CardContent>
-                <h1>Profile</h1>
-                <p>Editing your Profile will be available soon.</p>
+                <h1>Your Profile</h1>
+                {/* Todo: Find out what the blockchain imposed length limits are for each field */}
+                <TextField
+                  label="Name"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your display name"
+                  margin="normal"
+                  value={this.state.name}
+                  onChange={this.handleEditorChange_name}
+                  fullWidth
+                />
+                <TextField
+                  label="Profile description"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Profile description"
+                  margin="normal"
+                  value={this.state.about}
+                  onChange={this.handleEditorChange_about}
+                  fullWidth
+                />
+                <TextField
+                  label="Profile image"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Profile image"
+                  margin="normal"
+                  value={this.state.profile_image}
+                  onChange={this.handleEditorChange_profile_image}
+                  fullWidth
+                />
+                <TextField
+                  label="Cover image"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Cover image for your blog"
+                  margin="normal"
+                  value={this.state.cover_image}
+                  onChange={this.handleEditorChange_cover_image}
+                  fullWidth
+                />
+                <TextField
+                  label="Location"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your location"
+                  margin="normal"
+                  value={this.state.location}
+                  onChange={this.handleEditorChange_location}
+                  fullWidth
+                />
+                <TextField
+                  label="Website"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your website"
+                  margin="normal"
+                  value={this.state.website}
+                  onChange={this.handleEditorChange_website}
+                  fullWidth
+                />
+                <TextField
+                  label="Facebook"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your Facebook username or fanpage"
+                  margin="normal"
+                  value={this.state.facebook}
+                  onChange={this.handleEditorChange_facebook}
+                  fullWidth
+                />
+                <TextField
+                  label="Instagram"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your Instagram username"
+                  margin="normal"
+                  value={this.state.instagram}
+                  onChange={this.handleEditorChange_instagram}
+                  fullWidth
+                />
+                <TextField
+                  label="Youtube"
+                  inputProps={{
+                    maxLength: 100
+                  }}
+                  multiline={true}
+                  placeholder="Your Youtube username"
+                  margin="normal"
+                  value={this.state.youtube}
+                  onChange={this.handleEditorChange_youtube}
+                  fullWidth
+                />
               </CardContent>
             </Card>
-          </Grid>{" "}
+          </Grid>
         </Grid>
       </Fragment>
     );
   }
 }
+
+Profile.propTypes = {
+  user: PropTypes.string
+};
 
 export default Profile;

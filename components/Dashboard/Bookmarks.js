9549dbc6b470ffab60b75c89d6c582820c83b4a0
tiotdev <tiotdev@tiot.de>
2019-04-28 03:29:41 +0200

    Add bookmark queries & mutations

--- components/Dashboard/Bookmarks.js
+++ components/Dashboard/Bookmarks.js
@@ -1,35 +1,138 @@
-import React, { Fragment, Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Query } from "react-apollo";
+import { GET_BOOKMARKS } from "../../helpers/graphql/bookmarks";
 import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Helmet from "react-helmet";
+import GridPostCard from "../GridPostCard";
+import { imageProxy } from "../../helpers/getImage";
+import readingTime from "reading-time";
+import sanitize from "sanitize-html";
+import parseBody from "../../helpers/parseBody";
+import PropTypes from "prop-types";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { regExcerpt, regTitle } from "../../utils/regex";
+import InfiniteScroll from "react-infinite-scroller";
+import Head from "../Head";
 
-class Bookmarks extends Component {
+class PostGrid extends Component {
+  state = {
+    hasMore: true
+  };
+  noMore() {
+    this.setState({ hasMore: false });
+  }
   render() {
     return (
       <Fragment>
-        <Helmet>
-          <title>{"Bookmarks | TravelFeed: The Travel Community"}</title>
-        </Helmet>
-        <Grid
-          container
-          spacing={0}
-          alignItems="center"
-          justify="center"
-          className="pt-4 pb-4"
-        >
-          <Grid item lg={7} md={8} sm={11} xs={12}>
-            <Card>
-              <CardContent>
-                <h1>Bookmarks</h1>
-                <p>Bookmarking posts will be available soon.</p>
-              </CardContent>
-            </Card>
-          </Grid>{" "}
-        </Grid>
+        <Head title="Bookmarks" />
+        <Query query={GET_BOOKMARKS} variables={{ limit: 10 }}>
+          {({ data, loading, error, fetchMore }) => {
+            if (loading) {
+              return (
+                <Grid item lg={12} md={12} sm={12} xs={12}>
+                  <div className="p-5 text-center">
+                    <CircularProgress />
+                  </div>
+                </Grid>
+              );
+            }
+            if (error || data.bookmarks === null) {
+              return <Fragment />;
+            }
+            return (
+              <InfiniteScroll
+                loadMore={() =>
+                  fetchMore({
+                    variables: {
+                      offset: data.bookmarks.length
+                    },
+                    updateQuery: (prev, { fetchMoreResult }) => {
+                      if (fetchMoreResult.bookmarks.length === 0) {
+                        this.noMore();
+                      }
+                      if (!fetchMoreResult) return prev;
+                      return Object.assign({}, prev, {
+                        bookmarks: [
+                          ...prev.bookmarks,
+                          ...fetchMoreResult.bookmarks
+                        ]
+                      });
+                    }
+                  })
+                }
+                hasMore={this.state.hasMore}
+                threshold={1000}
+                loader={
+                  <Grid item lg={12} md={12} sm={12} xs={12}>
+                    <div className="p-5 text-center">
+                      <CircularProgress />
+                    </div>
+                  </Grid>
+                }
+              >
+                <Grid
+                  container
+                  spacing={0}
+                  alignItems="center"
+                  justify="center"
+                  className="p-3"
+                >
+                  {data.bookmarks.length > 0 &&
+                    data.bookmarks.map(post => {
+                      const imgHeight = 600;
+                      const htmlBody = parseBody(post.body, {});
+                      const sanitized = sanitize(htmlBody, { allowedTags: [] });
+                      const readtime = readingTime(sanitized);
+                      const image = imageProxy(post.img_url, "0x" + imgHeight);
+                      let title = regTitle(post.title);
+                      title =
+                        title.length > 85
+                          ? title.substring(0, 81) + "[...]"
+                          : title;
+                      const tags =
+                        post.tags && post.tags.length > 1
+                          ? [post.tags[1]]
+                          : ["travelfeed"];
+                      const excerpt = regExcerpt(sanitized);
+                      return (
+                        <Grid
+                          item
+                          lg={4}
+                          md={6}
+                          sm={12}
+                          xs={12}
+                          key={post.permlink}
+                        >
+                          <GridPostCard
+                            author={post.author}
+                            display_name={post.display_name}
+                            permlink={post.permlink}
+                            title={title}
+                            img_url={image}
+                            created_at={post.created_at}
+                            readtime={readtime}
+                            excerpt={excerpt}
+                            votes={post.votes}
+                            total_votes={post.total_votes}
+                            tags={tags}
+                            curation_score={post.curation_score}
+                            app={post.app}
+                            cardHeight={250}
+                            showBookmark={true}
+                          />
+                        </Grid>
+                      );
+                    })}
+                </Grid>
+              </InfiniteScroll>
+            );
+          }}
+        </Query>
       </Fragment>
     );
   }
 }
+PostGrid.propTypes = {
+  query: PropTypes.array.isRequired
+};
 
-export default Bookmarks;
+export default PostGrid;
--- components/Post/BookmarkIcon.js
+++ components/Post/BookmarkIcon.js
@@ -2,17 +2,128 @@ import React, { Fragment, Component } from "react";
 import PropTypes from "prop-types";
 import IconButton from "@material-ui/core/IconButton";
 import BookmarkIconBorder from "@material-ui/icons/BookmarkBorder";
-
-// Query mongo. If is in bookmarks...
+import BookmarkIconFilled from "@material-ui/icons/Bookmark";
+import { Mutation, Query } from "react-apollo";
+import {
+  ADD_BOOKMARK,
+  DELETE_BOOKMARK,
+  IS_BOOKMARKED
+} from "../../helpers/graphql/bookmarks";
+import Tooltip from "@material-ui/core/Tooltip";
 
 class BookmarkIcon extends Component {
   render() {
     return (
-      <IconButton>
-        <BookmarkIconBorder />
-      </IconButton>
+      <Fragment>
+        <Query
+          query={IS_BOOKMARKED}
+          variables={{
+            author: this.props.author,
+            permlink: this.props.permlink
+          }}
+        >
+          {({ data, loading, error }) => {
+            if (loading || error) {
+              return <Fragment />;
+            }
+            if (data.isBookmarked) {
+              return (
+                <IsBookmarked
+                  author={this.props.author}
+                  permlink={this.props.permlink}
+                />
+              );
+            }
+            return (
+              <IsNotBookmarked
+                author={this.props.author}
+                permlink={this.props.permlink}
+              />
+            );
+          }}
+        </Query>
+      </Fragment>
     );
   }
 }
+
+class IsBookmarked extends Component {
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_BOOKMARK}
+        variables={{
+          author: this.props.author,
+          permlink: this.props.permlink
+        }}
+      >
+        {(deleteBookmark, data) => {
+          if (data.data && data.data.deleteBookmark.success) {
+            return (
+              <IsNotBookmarked
+                author={this.props.author}
+                permlink={this.props.permlink}
+              />
+            );
+          }
+          return (
+            <Tooltip title="Remove bookmark" placement="bottom">
+              <IconButton onClick={deleteBookmark}>
+                <BookmarkIconFilled />
+              </IconButton>
+            </Tooltip>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
+
+class IsNotBookmarked extends Component {
+  render() {
+    return (
+      <Mutation
+        mutation={ADD_BOOKMARK}
+        variables={{
+          author: this.props.author,
+          permlink: this.props.permlink
+        }}
+      >
+        {(addBookmark, data) => {
+          if (data.data && data.data.addBookmark.success) {
+            return (
+              <IsBookmarked
+                author={this.props.author}
+                permlink={this.props.permlink}
+              />
+            );
+          }
+          return (
+            <Tooltip title="Add bookmark" placement="bottom">
+              <IconButton onClick={addBookmark}>
+                <BookmarkIconBorder />
+              </IconButton>
+            </Tooltip>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
+
+BookmarkIcon.propTypes = {
+  author: PropTypes.string,
+  permlink: PropTypes.string
+};
+
+IsNotBookmarked.propTypes = {
+  author: PropTypes.string,
+  permlink: PropTypes.string
+};
+
+IsBookmarked.propTypes = {
+  author: PropTypes.string,
+  permlink: PropTypes.string
+};
 
 export default BookmarkIcon;

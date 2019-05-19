c464578edba6e097eae81492c11ff2d9d928e6da
tiotdev <tiotdev@tiot.de>
2019-05-19 18:57:24 +0200

    Update config, add local config

--- config.js
+++ config.js
@@ -1,6 +1,25 @@
+import {
+  LOCALHOST_GMAPS_API_KEY,
+  LOCALHOST_MAPBOX_TOKEN
+} from "./config.local";
+
+// The API keys in this file are used client-side and restricted to travelfeed.io, so don't bother trying to steal them ;)
+export const GMAPS_API_KEY =
+  process.env.NODE_ENV == "production"
+    ? "AIzaSyCPxDdLuLnseopR4g3ClB2PvsZyiMBjS7c"
+    : LOCALHOST_GMAPS_API_KEY;
+export const MAPBOX_TOKEN =
+  process.env.NODE_ENV == "production"
+    ? "pk.eyJ1IjoidGlvdGRldiIsImEiOiJjanZ2NjVzdjQxZ3Q2M3ptczN5NTIwY3k4In0.ZIhYhbkfSAfbX11XDwI57w"
+    : LOCALHOST_MAPBOX_TOKEN;
+
+export const GRAPHQL_URL =
+  process.env.NODE_ENV == "production"
+    ? "https://api.travelfeed.io/graphql"
+    : "http://localhost:4000/graphql";
+
 export const DEFAULT_IMAGE = "";
 export const APP_VERSION = "travelfeed/0.2.5";
-export const GMAPS_JS_APIKEY = "AIzaSyCPxDdLuLnseopR4g3ClB2PvsZyiMBjS7c";
 
 var url = "http://localhost:3000/login";
 var rooturl = "http://localhost:3000";
@@ -11,12 +30,8 @@ if (process.env.NODE_ENV == "production") {
 export const STEEMCONNECT_CALLBACK_URL = url;
 export const ROOTURL = rooturl;
 
-export const GRAPHQL_URL = "http://localhost:4000/graphql";
-
 export const DEFAULT_META_DESCRIPTION =
   "Find inspiration for your travels on TravelFeed. Join the TravelFeed community, write your own travel blog and start earning!";
 
-export const author_blacklist = [];
-
-// E.g. post with copyright violations can be blacklisted here manually
-export const permlink_blacklist = [];
+// Cloudinary
+export const CLOUDINARY_CLOUD_NAME = "dmuksl47x";
---
+++ config.local.js
@@ -0,0 +1,3 @@
+// Insert your own API keys here for local development
+export const LOCALHOST_GMAPS_API_KEY = "";
+export const LOCALHOST_MAPBOX_TOKEN = "";

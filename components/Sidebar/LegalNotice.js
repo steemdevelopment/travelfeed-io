ab291f3988f733eeb25e1231138a0abe1540b123
tiotdev <tiotdev@tiot.de>
2019-07-28 14:00:20 +0200

    Add a hover styles

--- components/Sidebar/LegalNotice.js
+++ components/Sidebar/LegalNotice.js
@@ -5,27 +5,27 @@ const LegalNotice = () => {
   return (
     <div className="pt-4 p-2">
       <Link color="textPrimary" href="/about" passHref>
-        <a className="textSecondary">About</a>
+        <a className="textSecondary hoverline">About</a>
       </Link>
       {'　'}
       <Link color="textPrimary" href="/about/faq" passHref>
-        <a className="textSecondary">FAQ</a>
+        <a className="textSecondary hoverline">FAQ</a>
       </Link>
       {'　'}
       <Link color="textPrimary" href="/about/support-us" passHref>
-        <a className="textSecondary">Support Us</a>
+        <a className="textSecondary hoverline">Support Us</a>
       </Link>
       {'　'}
       <Link color="textPrimary" href="/about/terms" passHref>
-        <a className="textSecondary">Terms</a>
+        <a className="textSecondary hoverline">Terms</a>
       </Link>
       {'　'}
       <Link color="textPrimary" href="/about/privacy" passHref>
-        <a className="textSecondary">Privacy</a>
+        <a className="textSecondary hoverline">Privacy</a>
       </Link>
       {'　'}
       <Link color="textPrimary" href="/about/cookies" passHref>
-        <a className="textSecondary">Cookies</a>
+        <a className="textSecondary hoverline">Cookies</a>
       </Link>
       <p className="textSecondary pt-1">
         &copy; {new Date().getFullYear()} TravelFeed

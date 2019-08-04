c8cee589aae117647d3e0826257d18b45b934c98
tiotdev <tiotdev@tiot.de>
2019-08-04 16:44:18 +0200

    Create PayoutTypeSelector.js

---
+++ components/Editor/PayoutTypeSelector.js
@@ -0,0 +1,25 @@
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import React from 'react';
+
+const PayoutTypeSelector = props => {
+  const handleChange = value => {
+    props.onChange(value.target.value);
+  };
+
+  return (
+    <FormControl>
+      <InputLabel htmlFor="author-rewards">Author rewards</InputLabel>
+      <Select value={props.value} onChange={handleChange}>
+        <MenuItem value={false}>
+          50% liquid SBD/STEEM and 50% Steem Power
+        </MenuItem>
+        <MenuItem value>100% Steem Power</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+export default PayoutTypeSelector;

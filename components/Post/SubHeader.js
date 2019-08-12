44816caa5fc86954b5c73bfd95b6cf3fb7efcf18
tiotdev <tiotdev@tiot.de>
2019-08-12 13:26:22 +0200

    Move location to subheader, replace divider

--- components/Post/SubHeader.js
+++ components/Post/SubHeader.js
@@ -4,52 +4,84 @@ import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
+import { nameFromCC, slugFromCC } from '../../helpers/countryCodes';
+import Link from '../../lib/Link';
 
 dayjs.extend(relativeTime, LocalizedFormat); // use plugin
 
 const SubHeader = props => {
-  const { created_at, readtime } = props;
+  const { created_at, readtime, location } = props;
   const createdAt = dayjs(created_at);
-  const time = (
-    <Tooltip title={createdAt.format('MMMM DD YYYY H:mm')} placement="bottom">
-      <span>
-        {createdAt.isBefore(
-          dayjs()
-            .startOf('month')
-            .add(-1, 'month'),
-        )
-          ? createdAt.format('MMMM YYYY')
-          : createdAt.fromNow()}
-      </span>
-    </Tooltip>
-  );
-  let readingTime = <Fragment />;
-  if (readtime !== undefined) {
-    readingTime = (
-      <Fragment>
-        <span> | </span>
-        <Tooltip title={`${readtime.words} words`} placement="bottom">
-          <span>{readtime.text}</span>
-        </Tooltip>
-      </Fragment>
-    );
-  }
+
+  const country =
+    location.country_code !== null
+      ? nameFromCC(location.country_code)
+      : undefined;
+  const countryslug =
+    location.country_code !== null
+      ? slugFromCC(location.country_code)
+      : undefined;
 
   return (
     <Fragment>
-      {time}
-      {readingTime}
+      <Tooltip title={createdAt.format('MMMM DD YYYY H:mm')} placement="bottom">
+        <span>
+          {createdAt.isBefore(
+            dayjs()
+              .startOf('month')
+              .add(-1, 'month'),
+          )
+            ? createdAt.format('MMMM YYYY')
+            : createdAt.fromNow()}
+        </span>
+      </Tooltip>
+      {readtime && (
+        <Fragment>
+          <span> · </span>
+          <Tooltip title={`${readtime.words} words`} placement="bottom">
+            <span>{readtime.text}</span>
+          </Tooltip>
+        </Fragment>
+      )}
+      {country && (
+        <Fragment>
+          <span> · </span>
+          <Link
+            color="textSecondary"
+            as={`/destinations/${countryslug}/${
+              location.subdivision !== null ? location.subdivision : ''
+            }/`}
+            href={`/destinations?country=${countryslug}${
+              location.subdivision !== null
+                ? `&subdivision=${location.subdivision}`
+                : ''
+            }`}
+            passHref
+          >
+            <Tooltip
+              title={`${
+                location.subdivision !== null ? `${location.subdivision}, ` : ''
+              } ${country}`}
+              placement="bottom"
+            >
+              <span>{country}</span>
+            </Tooltip>
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
 
 SubHeader.defaultProps = {
   readtime: undefined,
+  location: {},
 };
 
 SubHeader.propTypes = {
   created_at: PropTypes.string.isRequired,
   readtime: PropTypes.objectOf(PropTypes.any),
+  location: PropTypes.objectOf(PropTypes.any),
 };
 
 export default SubHeader;
